70ed3cb841dfd30d6410d2ce40cfa1615ce13a76
Pallavimbhat <146982047+Pallavimbhat@users.noreply.github.com>
2026-09-04 10:33:44 +0530

    Time: 0 ms (100%), Space: 12.8 MB (45.39%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
-    int calPoints(vector<string>& a) {
-        stack<int> st;
-        int sum = 0;
-
-        for(int i = 0; i < a.size(); i++) {
-
-            if(a[i] == "+") {
-                int x = st.top();
-                st.pop();
-
-                int y = st.top();
-
+    int calPoints(vector<string>& operations) {
+        int sum=0;
+        stack<int>st;
+        for(int i=0;i<operations.size();i++){
+            if(operations[i]=="D"){
+                int x=st.top();
+                x*=2;
                 st.push(x);
-                st.push(x + y);
+                sum+=x;
             }
-
-            else if(a[i] == "D") {
-                int x = st.top();
-                st.push(2 * x);
-            }
-
-            else if(a[i] == "C") {
+            else if(operations[i]=="C"){
+                int x=st.top();
                 st.pop();
+                sum-=x;
             }
+            else if(operations[i]=="+"){
+                int x=st.top();
+                st.pop();
+                int y=st.top();
+                st.push(x);
+                int s=x+y;
+                st.push(s);
+                sum+=s;
 
+            }
             else {
-                st.push(stoi(a[i]));
+                sum+=stoi(operations[i]);
+                st.push(stoi(operations[i]));
             }
         }
-
-        while(!st.empty()) {
-            int temp = st.top();
-            st.pop();
-            sum += temp;
-        }
-
         return sum;
     }
 };
