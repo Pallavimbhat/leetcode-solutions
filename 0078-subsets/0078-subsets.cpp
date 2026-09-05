b82aa402cd57cb8ba9cee97b65b56d8430cd5280
Pallavimbhat <146982047+Pallavimbhat@users.noreply.github.com>
2026-09-05 19:46:37 +0530

    Time: 229 ms (3.47%), Space: 103.1 MB (5.04%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-  vector<vector<int>> ans;
-  vector<int> diary;
-   vector<vector<int>> solve(vector<int>& nums, int cur){
-    if(cur>=nums.size()){
-        ans.push_back(diary);
+    vector<vector<int>> solve(vector<int>& nums,int cur,int n,vector<vector<int>>& ans,vector<int>& diary){
+        if(cur==n){
+            ans.push_back(diary);
+            return ans;
+        }
+        solve(nums,cur+1,n,ans,diary);
+        diary.push_back(nums[cur]);
+        solve(nums,cur+1,n,ans,diary);
+        diary.pop_back();
         return ans;
     }
-    solve(nums,cur+1);
-    diary.push_back(nums[cur]);
-    solve(nums,cur+1);
-    diary.pop_back();
-    return ans;
-   }
     vector<vector<int>> subsets(vector<int>& nums) {
-        return solve(nums,0);
+        vector<vector<int>>ans;
+        vector<int>diary;
+        return solve(nums,0,nums.size(),ans,diary);
     }
 };
